d70221f6da4dfca4e456f36d4dc7231f7a054808
ved7997 <ved7997@gmail.com>
2022-01-29 18:57:48 +0300

    Третья попытка

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -4,15 +4,11 @@ import metod_class_1.Author;
 import metod_class_1.Book;
 
 public class Main {
-
     public static void main(String[] args) {
         Book vityMaleev1 = new Book("Витя Малеев в школе и дома", 1951);
         Book vityMaleev2 = new  Book();
         vityMaleev2.setYearRelease(1950);
-
         Author nosov = new Author("Николай", "Носов");
-
-
 
         System.out.println("Название книги: " + vityMaleev1.getTitleBook());
         System.out.println("Год издания: " + vityMaleev1.getYearRelease());
