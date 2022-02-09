8e7deaeba1f3d4ed7666dae132bafaf2890798f1
ved7997 <ved7997@gmail.com>
2022-02-09 22:33:21 +0300

    Сборщик мусора 5

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -2,22 +2,12 @@ package ru.skypro;
 
 import metod_class_1.Author;
 import metod_class_1.Book;
+import sborshik_mucora_08_02_22.MainMusor;
 
 public class Main {
     public static void main(String[] args) {
-        Book vityMaleev1 = new Book("Витя Малеев в школе и дома", 1951, new Author("Николай ", "Носов"));
-        Book vityMaleev2 = new  Book();
-        Author avthor = new Author("Николай", "Носов");
-        vityMaleev2.setYearRelease(1950);
-        vityMaleev2.toString();
-        System.out.println(vityMaleev1.toString());
-        System.out.println(vityMaleev1.hashCode());
-        System.out.println(avthor.hashCode());
-        System.out.println(avthor.equals(vityMaleev1));
-        System.out.println("Название книги: " + vityMaleev1.getTitleBook());
-        System.out.println("Год издания: " + vityMaleev1.getYearRelease());
-        System.out.println("Год издания: " + vityMaleev2.getYearRelease());
-        System.out.println("Автор: " + vityMaleev1.author.getAutorfirstName() + vityMaleev1.author.getAutorlastName());
+        int value = 33;
+        System.out.println(MainMusor.changeValue(value));
 
     }
 }
---
+++ src/sborshik_mucora_08_02_22/MainMusor.java
@@ -0,0 +1,13 @@
+package sborshik_mucora_08_02_22;
+
+public class MainMusor {
+
+    public static int changeValue(int value){
+      value = 22;
+      return value;
+
+
+    }
+
+
+}
